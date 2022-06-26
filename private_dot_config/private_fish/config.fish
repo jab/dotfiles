9f28b1457fee8ae2dce945eb84ea2b642566ee45
Joshua Bronson <jabronson@gmail.com>
2022-06-26 14:34:59 -0400

    Update config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,8 +2,9 @@ if status is-interactive
   set -gx EDITOR emacs
   set -gx LESS -iR
 
-  if test python
+  if test python || test python3
     set -gx PIP_REQUIRE_VIRTUALENV true
+
     if test -f ~/.local/pythonstartup.py &>/dev/null
       set -gx PYTHONSTARTUP ~/.local/pythonstartup.py
     end
@@ -19,5 +20,70 @@ if status is-interactive
 
   if type -q starship  # https://starship.rs
     starship init fish | source
+  end
+end
+
+function mcd -a dir
+  mkdir -p $dir && cd $dir
+end
+
+function add_abbrevs
+  abbr -a -- c command
+  abbr -a -- cp 'cp -iv'
+  abbr -a -- mv 'mv -iv'
+  abbr -a -- rm 'rm -iv'
+
+  abbr -a -- e emacs
+  abbr -a -- v vim
+
+  abbr -a -- g git
+  abbr -a -- gst 'git status'
+  abbr -a -- gsw 'git switch'
+  abbr -a -- gdi 'git diff'
+  abbr -a -- gad 'git add --all'
+  abbr -a -- gci 'git commit'
+  abbr -a -- gps 'git push'
+  abbr -a -- gpl 'git pull'
+  abbr -a -- grs 'git restore'
+  abbr -a -- gco 'git checkout'
+
+  if type -q chezmoi  # https://chezmoi.io
+    abbr -a -- ch chezmoi
+  end
+
+  if type -q rip  # https://github.com/nivekuil/rip
+    abbr -a -- r rip
+  end
+
+  if type -q duf
+    abbr -a -- df duf
+  end
+
+  if type -q ncdu
+    abbr -a -- du ncdu
+  end
+
+  if type -q fd
+    abbr -a -- find fd
+  end
+
+  if type -q gping
+    abbr -a -- ping gping
+  end
+
+  if type -q procs
+    abbr -a -- ps procs
+  end
+
+  if type -q sd
+    abbr -a -- sed sd
+  end
+
+  if type -q btm
+    abbr -a -- top btm
+  end
+
+  if type -q br
+    abbr -a -- tree 'br -s'
   end
 end
